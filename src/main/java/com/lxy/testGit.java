38a52172353b4efe49f71226f2762e33e5ce0219
lxy <2044430307@qq.com>
2023-05-19 19:51:11 +0800

    master second commit

--- src/main/java/com/lxy/testGit.java
+++ src/main/java/com/lxy/testGit.java
@@ -2,6 +2,8 @@ package com.lxy;
 
 public class testGit {
     public static void main(String[] args) {
+        System.out.println("12222222");
+        System.out.println("22222222222");
 
     }
 }
